3531d842b911a8bf0147a746b93a888622fd3c7a
ElenaSh7 <elspers.acc@gmail.com>
2023-12-16 11:17:45 -0500

    exercise 2

---
+++ lesson-01/03.js
@@ -0,0 +1,11 @@
+
+let number = 1;
+
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
+
+//используя лишь
+// --number   number--  ++number   number++
+// Получи в консоли 1,3,3,1
