7256a40f1142eda4925dcc614843291c1639b141
ElenaSh7 <elspers.acc@gmail.com>
2023-12-16 12:35:50 -0500

    exercise 3

---
+++ lesson-01/07.js
@@ -0,0 +1,13 @@
+let iAm = 'я'
+let krasavchik = "красавчик!";
+
+console.log(iAm.toUpperCase() + ' ' + krasavchik);
+
+
+
+//Дано:
+//let iAm = 'я'
+// let krasavchik = "красавчик!";
+
+//   Выведи в консоль «Я красавчик!».
+// ❗ Обрати внимание: «Я» с заглавной.
